931a4d86c7bf952c22387b0ea516ee104f31f869
Sherbb <alansherba@gmail.com>
2023-11-18 21:15:29 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="absolute h-full w-full bg-black p-4 flex flex-row gap-8">
+      <div className="w-[512px]">
+        <div className="rounded-[32px] bg-black p-8 border-2 border-[#30302B]">
+          <p className="font-light text-4xl">Alan Sherba</p>
+          <div className="h-4" />
+          <p className="font-light text-base">Programmer</p>
+          <p className="font-light text-base">
+            UX, Feel, Action, Game Designer
+          </p>
+          <p className="font-light text-base">Unity Engineer</p>
+          <p className="font-light text-base">VFX Artist</p>
+        </div>
+      </div>
+
+      <div className="h-full w-[2px] bg-[#30302B4D]" />
+
+      <div className="w-[512px] pt-8">
+        <p className="font-light text-4xl">Alan Sherba</p>
+        <div className="h-4" />
+        <p className="font-light text-base">Programmer</p>
+        <p className="font-light text-base">UX, Feel, Action, Game Designer</p>
+        <p className="font-light text-base">Unity Engineer</p>
+        <p className="font-light text-base">VFX Artist</p>
+      </div>
+
+      <div className="h-full w-[2px] bg-[#30302B4D]" />
+
+      <div className="grow grid place-items-end">
+        <div className="text-right">
+          <p className="font-ligh right-0 text-4xl">Contact</p>
+          <p className="font-light text-base">alansherba@gmail.com</p>
+        </div>
+      </div>
     </div>
   );
 }
